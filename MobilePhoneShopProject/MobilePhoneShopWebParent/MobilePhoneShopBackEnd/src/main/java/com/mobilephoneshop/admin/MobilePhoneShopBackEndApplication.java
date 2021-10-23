f0d9382b33cefb39b70855f052e7a28d7b11a78f
Duc Khai <duckhai0623@gmail.com>
2021-10-23 09:33:56 +0700

    add MobilePhoneShopBackEnd

---
+++ MobilePhoneShopProject/MobilePhoneShopWebParent/MobilePhoneShopBackEnd/src/main/java/com/mobilephoneshop/admin/MobilePhoneShopBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.mobilephoneshop.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobilePhoneShopBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobilePhoneShopBackEndApplication.class, args);
+	}
+
+}
