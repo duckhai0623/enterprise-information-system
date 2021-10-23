1184a5127378f339cd6280ed3097803464994da1
Duc Khai <duckhai0623@gmail.com>
2021-10-23 09:53:29 +0700

    add index web for admin and home page

---
+++ MobilePhoneShopProject/MobilePhoneShopWebParent/MobilePhoneShopFrontEnd/src/main/java/com/mobilephoneshop/sites/MobilePhoneShopFrontEndApplication.java
@@ -0,0 +1,13 @@
+package com.mobilephoneshop.sites;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobilePhoneShopFrontEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobilePhoneShopFrontEndApplication.class, args);
+	}
+
+}
---
+++ MobilePhoneShopProject/MobilePhoneShopWebParent/MobilePhoneShopFrontEnd/src/test/java/com/mobilephoneshop/MobilePhoneShopFrontEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mobilephoneshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobilePhoneShopFrontEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
